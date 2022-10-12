9fe7b48126fb4da1efeff36b8845de9a6e78c9ea
Brian Santarelli <bsantarelli@collegeboard.org>
2022-10-12 11:29:38 -0400

    chore: refinement of lazyinit problem statement and test harness

--- lazyinit/src/index.test.ts
+++ lazyinit/src/index.test.ts
@@ -1,4 +1,19 @@
+import { calculateOrGetExpensiveValue } from './index';
+
 describe('lazy-init tests', () => {
-    it('`calculateOrGetExpensiveValue` only initializes once for concurrent invocations', async () => {
-    })
-})
+  beforeEach(() => {
+    // Make Jest use real timers for this test case otherwise `setTimeout` will not work as expected
+    jest.useRealTimers();
+  });
+
+  afterEach(() => {
+    // Back to default
+    jest.useFakeTimers();
+  });
+
+  it('only initializes once', async () => {
+    const promiseResults = await Promise.all([calculateOrGetExpensiveValue(), calculateOrGetExpensiveValue() , calculateOrGetExpensiveValue() , calculateOrGetExpensiveValue(), calculateOrGetExpensiveValue()]);
+
+    expect(promiseResults).toEqual([promiseResults[0], promiseResults[0], promiseResults[0], promiseResults[0], promiseResults[0]]);
+  });
+});
--- lazyinit/src/index.ts
+++ lazyinit/src/index.ts
@@ -1,21 +1,25 @@
 let someExpensivelyCalculatedValue: string;
 
-const sleep = async (timeMs: number) => {
-    await new Promise((resolve) => setTimeout(() => {
-        resolve(undefined)
-    }, timeMs))
-}
+export const sleep = async (sleepMs: number) => {
+  return new Promise(resolve => setTimeout(resolve, sleepMs));
+};
+
+const doSomethingExpensive = async (): Promise<string> => {
+  const expensiveValue = new Date(Date.now() + Math.random() * 5000).toISOString();
+  await sleep(250);
+  return expensiveValue;
+};
 
 /**
  * This function lazily initializes (caches) an expensive value to calculate.  The provided implementation is not
- * concurrency-safe in the sense that concurrent calls to `calculateOrGetExpensiveValue` will cause the
- * expensive value to be recalculated.
+ * concurrency-friendly in the sense that concurrent execution of async calls to `calculateOrGetExpensiveValue`
+ * will cause the expensive value to be calculated multiple times if it has not yet been cached.
  *
  * Modify this code so that the expensive value is only calculated once regardless of how many concurrent
  * async calls are made to this function.  You _may not_ utilize external libraries to solve this issue.
  *
- * The accompanying test case should be implemented first to 1) prove there is an issue and 2) after changes,
- * prove that a workable solution has been implemented
+ * The accompanying test case will fail with this implementation.  Your augmented implementation should make
+ * the test case pass :)
  */
 export const calculateOrGetExpensiveValue = async () => {
     if (!someExpensivelyCalculatedValue) {
@@ -23,10 +27,5 @@ export const calculateOrGetExpensiveValue = async () => {
         someExpensivelyCalculatedValue = await doSomethingExpensive();
     }
     return someExpensivelyCalculatedValue;
-}
+};
 
-export const doSomethingExpensive = async (): Promise<string> => {
-    const expensiveValue = new Date(Date.now() + Math.random() * 5000).toISOString();
-    await sleep(1000);
-    return expensiveValue;
-}
